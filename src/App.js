d8767613b9ee48ab27c744ae3ba675e7ca3fad06
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-05-02 18:24:12 -0400

    Created initial App.css and index.css properties

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./App.css";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  What,
+} from "./containers";
+import { Brand, CTA, Navbar } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <h1>DigitalDT</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <What />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
