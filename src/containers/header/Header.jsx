27c98777970c096a0f4500f092f4e0e287ce3fab
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-05-03 18:08:52 -0400

    Header section complete

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header section__padding" id="home">
+      <div className="header-content">
+        <h1 className="gradient__text">
+          Let's build something amazing with GPT-3 Open AI
+        </h1>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+        <div className="header-content__input">
+          <input type="email" placeholder="Your email address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access in the last 24 hours</p>
+        </div>
+      </div>
+      <div className="header-image">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
