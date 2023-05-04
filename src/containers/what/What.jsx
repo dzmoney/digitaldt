e1ef3c18b2c02edf4e6fa68718c149a6bea7083b
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-05-03 21:11:58 -0400

    Finished What and Feature components

--- src/containers/what/What.jsx
+++ src/containers/what/What.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./what.css";
+import { Feature } from "../../components";
 
 const What = () => {
-  return <div>What</div>;
+  return (
+    <div className="what section__margin" id="what">
+      <div className="what-feature">
+        <Feature
+          title="What is GPT-3?"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
+      </div>
+      <div className="what-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="what-container">
+        <Feature
+          title="Chatbots"
+          text="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        />
+        <Feature
+          title="Knowledgebase"
+          text="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        />
+        <Feature
+          title="Education"
+          text="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default What;
