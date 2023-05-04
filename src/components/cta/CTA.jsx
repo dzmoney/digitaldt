a5574e3d980eaec43fc2a5b158b5fc25cd755e03
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-05-04 17:53:11 -0400

    Possibility container finished

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./cta.css";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="possibility-block">
+      <div className="possibility-block__text">
+        <p>Request Early Access to Get Started</p>
+        <h2>Register today & start exploring the endless possiblities.</h2>
+      </div>
+      <button type="button">Get Started</button>
+    </div>
+  );
 };
 
 export default CTA;
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./possibility.css";
+import possibility from "../../assets/possibility.png";
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="possibility section__padding" id="possibility">
+      <div className="possibility-image">
+        <img src={possibility} alt="possibility-img" />
+      </div>
+      <div className="possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
