1280fe9547f69d6145caf78fe4e9306649e96256
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-05-04 18:26:44 -0400

    CTA component done

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,12 +3,14 @@ import "./cta.css";
 
 const CTA = () => {
   return (
-    <div className="possibility-block">
-      <div className="possibility-block__text">
+    <div className="CTA">
+      <div className="CTA-content">
         <p>Request Early Access to Get Started</p>
         <h2>Register today & start exploring the endless possiblities.</h2>
       </div>
-      <button type="button">Get Started</button>
+      <div className="CTA-btn">
+        <button type="button">Get Started</button>
+      </div>
     </div>
   );
 };
